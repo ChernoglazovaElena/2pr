fc708faa835ff7e445e75d7b631be98b63d72382
Елинка <113973001+ChernoglazovaElena@users.noreply.github.com>
2023-03-21 21:22:44 +0300

    Add files via upload

---
+++ secondPr/Page1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using secondPr.bdDataSetTableAdapters;
+
+namespace secondPr
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        shoesTableAdapter shoes = new shoesTableAdapter();
+        public Page1()
+        {
+            InitializeComponent();
+            MyGrid.ItemsSource = shoes.GetData();
+        }
+
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+            int id = shoes.GetData().Count - 1;
+            shoes.InsertQuery(id, Convert.ToInt32(NUU.Text));
+            MyGrid.ItemsSource = shoes.GetData();
+        }
+    }
+}
---
+++ secondPr/Page2.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using secondPr.bdDataSetTableAdapters;
+
+namespace secondPr
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        clothesTableAdapter clothes = new clothesTableAdapter();
+        public Page2()
+        {
+            InitializeComponent();
+            MyGrid.ItemsSource = clothes.GetData();
+        }
+
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+            int id = clothes.GetData().Count - 1;
+            clothes.InsertQuery(NUU.Text);
+            MyGrid.ItemsSource = clothes.GetData();
+        }
+    }
+}
